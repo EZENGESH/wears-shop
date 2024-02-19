b27dbf5cc41c454622440363e60f7e1021277f1a
EZENGESH <ezengesh@gmail.com>
2024-02-19 22:14:12 +0300

    moving file to remote

---
+++ Data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+    {
+      id: "A1",
+      name: "Casual Shirt",
+      price: 4500,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-1.jpg",
+    },
+    {
+      id: "A2",
+      name: "Office Shirt",
+      price: 1000,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-2.jpg",
+    },
+    {
+      id: "A3",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-3.jpg",
+    },
+    {
+      id: "A4",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-4.jpg",
+    },
+    {
+        id: "A5",
+        name: "Mens Suit",
+        price: 300,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/image-5.jpg",
+      },
+  ];
